1299c9b3a4a50f36854a5eb7640b548df38df65d
Samba SIDIBE <sambasidibe@gmail.com>
2026-02-06 10:00:29 +0000

    init projet

---
+++ src/main/java/sn/edu/isepat/vente_chaussure/VenteChaussureApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.vente_chaussure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteChaussureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteChaussureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepat/vente_chaussure/VenteChaussureApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.vente_chaussure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteChaussureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
